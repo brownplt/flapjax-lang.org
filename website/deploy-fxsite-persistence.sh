0e45876b7aecd3a5988a97e4dbc5a4fd00f6a2ed
Arjun Guha <arjun.guha@gmail.com>
2012-12-07 08:35:55 -0500

    brown changed auth

--- website/deploy-fxsite-persistence.sh
+++ website/deploy-fxsite-persistence.sh
@@ -2,9 +2,9 @@
 
 if [ `hostname -d` = "cs.brown.edu" ]; then
   rsync -rltovz --safe-links --delete build/ \
-    flapjax@flapjax-vista:/home/flapjax/web/static
+    flapjax-vista:/home/flapjax/web/static
   rsync -rltovz --safe-links --delete persistence/ \
-    flapjax@flapjax-vista:/home/flapjax/web/persistence
+    flapjax-vista:/home/flapjax/web/persistence
 else
   echo "ERROR: you must deploy from a cs.brown.edu machine."
 fi
